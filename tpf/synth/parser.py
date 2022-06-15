59b01f8e0b530f5c3493eb100858d3286b93a88a
MaxiSim <101142571+MaxiSim@users.noreply.github.com>
2022-06-15 14:18:13 -0300

    muevo functions a la carpeta synth

--- tpf/synth/parser.py
+++ tpf/synth/parser.py
@@ -3,14 +3,13 @@ import numpy as np
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--name', type=str, help='name of someone to salute')
-    parser.add_argument('-t', dest= 'repetitions', type=int, help='times to salute')
+    parser.add_argument('-p', '--music_sheet', type=str, help='file with music sheet')
+    parser.add_argument('-i', '--instrument', type=str, help='file with instrument')
+    parser.add_argument('-o', '--output', type=str, help='wavefile with output')
+    parser.add_argument('-f', '--frequency', type=int, help='frequency of output')
     args = parser.parse_args()
-    for i in range(args.repetitions):
-        print(f'Hola {args.name}')
 
-    print(np.random.random())
-    
+    print(args)
 
 if __name__ == '__main__':
     main()
---
+++ tpf/synth/sintetizador.py
@@ -0,0 +1,6 @@
+
+class synth():
+    def __init__ (self, freq, dur, amp):
+        self.freq = freq
+        self.dur = dur
+        self.amp = amp
