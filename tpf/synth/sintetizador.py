cbec147a445357e4e8a6c04b6525ec311ed5ab12
Ignacio Villar Rodriguez <villarrodriguezi@udesa.edu.ar>
2022-07-02 17:08:14 -0300

    Notas diccionario

--- tpf/synth/sintetizador.py
+++ tpf/synth/sintetizador.py
@@ -1,15 +1,13 @@
 from instrument import Instrument
+import numpy as np
 
-class synth():
+
+class song():
     def __init__ (self, freq, sheet, instrument):
         self.freq = freq
         self.sheet = sheet
         self.instrument = instrument
         
-    def synthetise(self, sheet, instrument):
-        with open(sheet, 'r') as file:
-            for note in file:
-                line = note.split(' ')
-                for harmonic in range(1, instrument.harmonics):
-                    print()
-        
+   
+    
+song_data = np.array()
