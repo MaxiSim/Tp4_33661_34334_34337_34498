d1d0abbf784a7598a871bc1f93e4f2d7c45cdf37
MaxiSim <101142571+MaxiSim@users.noreply.github.com>
2022-07-05 20:30:22 -0300

    arregla los tests

--- synth-main/synth/test_functions.py
+++ synth-main/synth/test_functions.py
@@ -27,7 +27,7 @@ def test_quartcos():
     if functions.quartcos(0, 1) != 1:
         assert False
 def test_quartsin():
-    if functions.quartsin(1, 2) != (np.sin(np.pi / 2 * 2)):
+    if functions.quartsin(1, 2) != np.sin(np.pi / 4):
         assert False
 def test_halfcos():
     if functions.halfcos(0, 1) != 1:
@@ -45,10 +45,10 @@ def test_invlog2():
     if functions.invlog(2, 3) != np.log10(4):
         assert False
 def test_tri1():
-    if functions.tri(2, 1, 3, 1.5) != (2/3):
+    if functions.tri(1, 2, 0.7, 1) != 1:
         assert False 
 def test_tri2():
-    if functions.tri(4, 2, 3, 1) != (5/2):
+    if functions.tri(1, 2, 3, 1) != (1/3):
         assert False
 def test_tri3():
     if functions.tri(1, 2, 0.7, 1) != 1:
